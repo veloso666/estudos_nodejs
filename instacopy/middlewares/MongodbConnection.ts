e82fbba0e091205c48361f16a1338843953577b7
João lucas <jlvatejo123@gmail.com>
2023-08-08 01:18:06 -0300

    schemas, autenticação e integração com banco de dados

--- instacopy/middlewares/MongodbConnection.ts
+++ instacopy/middlewares/MongodbConnection.ts
@@ -1,26 +1,30 @@
 import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
-import mongoose  from 'mongoose';
-export const connectorMongodb = (handler : NextApiHandler) =>
-   async (req: NextApiRequest, res : NextApiResponse) =>{
-        /*verifica se o banco esta conectado,
-         se estiver, seguir para o endpoint ou proximo middleware */
-        if(mongoose.connections[0].readyState){
-            return handler(req, res);
-        }
-        /*se nao estiver conectado, vamos conectar
-        obter a variavel de ambiente preenchida do env
-        */
-       const {DB_CONEXAO_STRING} = process.env
+import mongoose from 'mongoose';
+import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
 
-       //se a env estiver vazia, aborta o uso do sistema, e avisa o programador
-        if(!DB_CONEXAO_STRING){
-            return res.status(500).json({erro : 'ENV de configuracao do banco, nao informada'});
-            }
-            mongoose.connection.on('connected', ()=> console.log('Banco de dados conectado'))
-            mongoose.connection.on('error', error => console.log(`Ocorreu um erro ao conectar: ${error}`))
-           await mongoose.connect(DB_CONEXAO_STRING);
+export const conectarMongoDB = (handler : NextApiHandler) =>
+    async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
-           //agora pode seguir para o endpoint, pois estou conectado no banco
-           return handler(req, res)
-        }
+    // verificar se o banco ja esta conectado, se estiver seguir 
+    // para o endpoint ou proximo middleware
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
+    }
+
+    // ja que nao esta conectado vamos conectar
+    // obter a variavel de ambiente preenchida do env
+    const {DB_CONEXAO_STRING} = process.env;
+
+    // se a env estiver vazia aborta o uso do sistema e avisa o programador
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({ erro : 'ENV de configuracao do banco, nao informado'});
+    }
+
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+    await mongoose.connect(DB_CONEXAO_STRING);
     
+    // agora posso seguir para o endpoint, pois estou conectado
+    // no banco
+    return handler(req, res);
+}
--- instacopy/pages/api/login.ts
+++ instacopy/pages/api/login.ts
@@ -1,21 +1,25 @@
 import type {NextApiRequest, NextApiResponse} from "next";
-import {connectorMongodb} from '../../middlewares/MongodbConnection'
+import {conectarMongoDB} from '../../middlewares/MongodbConnection'
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import md5 from 'md5'
+import { UsuarioModel } from "@/models/UsuarioModel";
 
-const endpointLogin = (
+const endpointLogin = async(
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' && 
-            senha === 'Admin@123'){
-                return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+        const usuariosEncontrados = await UsuarioModel.find({email : login,senha : md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+
+            const usuarioEncontrado = usuariosEncontrados[0];
+                return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`});
         }
         return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
     }
     return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
 
-export default connectorMongodb(endpointLogin);
+export default conectarMongoDB(endpointLogin);
